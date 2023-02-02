73e454dbec381be3fd31abab8fdcf3d07375b2ad
Rinat Muratidinov <rmuratidinov@it-one.ru>
2023-02-02 19:21:35 +0600

    init

---
+++ src/main/kotlin/kg/rinat/sudary/SudaryApplication.kt
@@ -0,0 +1,11 @@
+package kg.rinat.sudary
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SudaryApplication
+
+fun main(args: Array<String>) {
+    runApplication<SudaryApplication>(*args)
+}
---
+++ src/test/kotlin/kg/rinat/sudary/SudaryApplicationTests.kt
@@ -0,0 +1,13 @@
+package kg.rinat.sudary
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SudaryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
